7e3a5156b23dbd9fe391d5a6866f71fb61daad9b
Farhan Khan <farhan.k2005@gmail.com>
2023-12-28 18:44:21 -0500

    created menu method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        String userChoice;
+
+        //print menu
+        printSignedOutMenu();
+
         
+    }
+
+    private static void printSignedOutMenu(){
+
+        System.out.println("\n\t\t\tWelcome");
+        System.out.println("\t\t------------------------\n");
+
+        System.out.println("Please selected one of the following options:\n");
+        System.out.println("a. Login \nb. Create new account\nq. Quit\n");
     }
 }
